3e1f96bba7e73df3f6d7b8c0fd0fb86d1f2755fa
Sudhin Varghese <sudh_chandy@yahoo.com>
2013-07-01 12:34:29 -0400

    Removed unncessary code

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -4,34 +4,21 @@
 angular.module('itemDetails.controllers', ['itemDetails.services']).
     controller('itemCtrl', function ($scope, $http, itemModel) {
     
-        $scope.options = itemModel.getOptions();
-        $scope.currentOption = $scope.options[0];
-        $scope.details = [];
-
-        /** change current option, get data for table */
+    	$scope.details = [];
+    	
+    	/** change current option, get data for table */
         $scope.setCurrentOption = function (option) {
             $scope.currentOption = option;
             $scope.fetchData();
         };
-        
-        $scope.formatResponse = function (data) {
-        	var allItems=[];
-        	allItems.length = data.length;
-        	var i;
-        	for (i = 0; i < allItems.length; i++) {
-        		allItems[i] = {
-        			name:data[i].name,
-        			value:data[i].value
-        		};
-        	}
-        	return allItems;
-        };
-
-        $scope.fetchData = function () {
+    
+    	$scope.fetchData = function () {
         	
             $http.get('/'+$scope.currentOption.value).then(function(response) {
-              $scope.details = $scope.formatResponse(response.data);
+              $scope.details = response.data;
 		    });
         };
-
+    
+        $scope.options = itemModel.getOptions();
+        $scope.setCurrentOption($scope.options[0]);
     });
